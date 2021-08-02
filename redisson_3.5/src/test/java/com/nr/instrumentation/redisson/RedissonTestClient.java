291dcf12d379b743af2aa37547bcd19f29310efe
Doug Hilpipre <dhilpipre@users.noreply.github.com>
2021-08-02 09:48:23 -0500

    added embedded server for testing

--- redisson_3.5/src/test/java/com/nr/instrumentation/redisson/RedissonTestClient.java
+++ redisson_3.5/src/test/java/com/nr/instrumentation/redisson/RedissonTestClient.java
@@ -3,9 +3,12 @@ package com.nr.instrumentation.redisson;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.Collection;
 //import java.util.List;
 import java.util.Map;
+import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
 import javax.management.RuntimeErrorException;
@@ -31,21 +34,55 @@ import com.newrelic.api.agent.Trace;
 
 import reactor.rx.Promise;
 import reactor.rx.Streams;
+import redis.embedded.RedisServer;
 
 @RunWith(InstrumentationTestRunner.class)
 @InstrumentationTestConfig(includePrefixes = { "org.redisson" })
 public class RedissonTestClient {
 
 	static RedissonClient client = null;
-	static RedissonReactiveClient reactive = Redisson.createReactive();
+	static RedissonReactiveClient reactive = null;
+	protected static RedisServer redisServer = null;
 	
 	@BeforeClass
-	public static void beforeClass() {
+	public static void beforeClass() throws IOException {
+		InputStream stream = RedissonTestClient.class.getResourceAsStream("/redis.properties");
+		if (stream != null) {
+			System.out.println("Opened inputstream to redis.properties");
+		} else {
+			System.out.println("Failed to open inputstream to redis.properties");
+		}
+		Properties properties = new Properties();
+		if (stream != null) {
+			properties.load(stream);
+		}
+
+		boolean useEmbedded = Boolean.parseBoolean(properties.getProperty("redis-server-embedded", "true"));
+
+		String host = properties.getProperty("redis-server-host", "localhost");
+		int port = Integer.parseInt(properties.getProperty("redis-server-port", "6379"));
+
+		if (useEmbedded) {
+			redisServer = new RedisServer(port);
+			redisServer.start();
+			System.out.println("Embedded Redis Server started on port " + port);
+		} else {
+			System.out.println("Will use external Redis server on " + host + " and port " + port);
+		}
+		try {
+			Thread.sleep(5000L);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 		client = Redisson.create();
+		reactive = Redisson.createReactive();
 	}
 	
 	@AfterClass
 	public static void afterClass() {
+		if(redisServer != null) {
+			redisServer.stop();
+		}
 	}
 	
 	@Test
